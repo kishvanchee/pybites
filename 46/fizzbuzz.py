19600bb1b3519aeee97a244ebc521a4f05daaeb4
Kishore Vancheeshwaran <kishore.vancheeshwaran@gmail.com>
2020-12-02 23:38:56 +0530

    submission Bite 46 @ codechalleng.es

---
+++ 46/fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz(num):
+    if num % 3 == 0:
+        if num % 5 == 0:
+            return 'Fizz Buzz'
+        else:
+            return 'Fizz'
+    else:
+        if num % 5 == 0:
+            return 'Buzz'
+        else:
+            return num
