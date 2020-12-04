cb5f2d87058bb84efe9029cf1b1adf4df62bccfc
Kishore Vancheeshwaran <kishore.vancheeshwaran@gmail.com>
2020-12-04 15:22:54 +0530

    submission Bite 6 @ codechalleng.es

---
+++ 6/pcc_stats.py
@@ -0,0 +1,76 @@
+from collections import Counter, namedtuple
+import os
+import urllib.request
+
+# prep
+tmp = os.getenv("TMP", "/tmp")
+tempfile = os.path.join(tmp, 'dirnames')
+urllib.request.urlretrieve(
+    'https://bites-data.s3.us-east-2.amazonaws.com/dirnames.txt',
+    tempfile
+)
+
+IGNORE = 'static templates data pybites bbelderbos hobojoe1848'.split()
+
+Stats = namedtuple('Stats', 'user challenge')
+
+
+# code
+
+def gen_files(tempfile=tempfile):
+    """
+    Parse the tempfile passed in, filtering out directory names
+    (first column) using the last "is_dir" column.
+
+    Lowercase these directory names and return them as a generator.
+
+    "tempfile" has the following format:
+    challenge<int>/file_or_dir<str>,is_dir<bool>
+
+    For example:
+    03/rss.xml,False
+    03/tags.html,False
+    03/Mridubhatnagar,True
+    03/aleksandarknezevic,True
+
+    => Here you would return 03/mridubhatnagar (lowercased!)
+       followed by 03/aleksandarknezevic
+    """
+    with open(tempfile, 'r') as f:
+        #ff = (line.lower().split(',')[0] for line in f if 'True' in line)
+        #yield ff
+        for line in f:
+            if 'True' in line:
+                yield line.lower().split(',')[0]
+
+
+
+def diehard_pybites(files=None):
+    """
+    Return a Stats namedtuple (defined above) that contains:
+    1. the user that made the most pull requests (ignoring the users in IGNORE), and
+    2. a tuple of:
+        ("most popular challenge id", "amount of pull requests for that challenge")
+
+    Calling this function on the default dirnames.txt should return:
+
+    Stats(user='clamytoe', challenge=('01', 7))
+    """
+    if files is None:
+        files = gen_files()
+
+    users = Counter()
+    popular_challenges = Counter()
+
+    # your code
+
+    for f in files:
+        user = f.split('/')[1]
+        challenge = f.split('/')[0]
+        if user in IGNORE:
+            continue
+        users[user] += 1
+        popular_challenges[challenge] += 1
+
+
+    return Stats(users.most_common(1)[0][0], *popular_challenges.most_common(1))
